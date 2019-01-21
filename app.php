6d08cff84a42056af52d3201ed46323239f5b36b
Thijs <44265614+jtdevries@users.noreply.github.com>
2019-01-21 09:56:43 +0100

    testing php

---
+++ app.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>first php app</title>
+  </head>
+  <body>
+
+    <?php
+    echo "Hello World!";
+    ?>
+
+  </body>
+</html>
